6627ddfb82af758e48681ba88d3fba13541eceb7
marlondemas <demasmarlon@gmail.com>
2021-03-09 19:55:38 +0200

    React scaffold with mock data

---
+++ components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Posts from './Posts'
+
+function App() {
+    return (
+        <div className="app_container">
+            <Posts />
+        </div>
+    )
+}
+
+export default App;
